1daebf4a1b8a8a3b2c34a9fb8784b3a61aac01d0
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-21 00:42:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
 def my_each(array)
   counter = 0
   while counter < array.length
-    array[counter]
+    yield popcorn(counter)
     counter += 1
   end
   array
+end
+
+
+def popcorn(array2)
+  return array[counter]
 end
