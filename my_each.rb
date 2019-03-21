aca0f1c2039862efe652c393775a588454ddc85c
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-21 00:57:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
-  c = 0
-  while c < array.length
-    yield(array[c])
-    c = c + 1
+  counter = 0
+  while counter < array.length
+    yield(array[counter])
+    counter += 1
   end
   array
 end
